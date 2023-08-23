62f35b295d219af79b2bdb1fbff5925d1f4dad2f
Ching0810 <neinchin123@gmail.com>
2023-08-23 18:55:11 +0800

    note: add note

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+// app is a function
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  // req & res are both object, therefore we could use method through .
   res.send('express app for URL_shorten')
 })
 
