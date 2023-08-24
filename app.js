efcc4d2551d4c825872f48d5d12f7c65c8f7ffdb
Ching0810 <neinchin123@gmail.com>
2023-08-24 14:45:03 +0800

    build: use express-hbs as view engine & 5 random code function

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
+// import express from 'express'
+// import { engine } from 'express-handlebars'
 // app is a function
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// app.set('views', './views');
+
 app.get('/', (req, res) => {
   // req & res are both object, therefore we could use method through .
-  res.send('express app for URL_shorten')
+  res.redirect('/url_shorten')
+})
+
+function random () {
+  arr = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  let str = ''
+  for (let i = 0; i < 5; i++) {
+    str += arr[Math.floor(Math.random() * 62)].toString()
+  }
+  return str
+}
+
+app.get('/url_shorten', (req, res) => {
+  res.render('index', {shorten: random()})
 })
 
 app.listen(port, () => {
