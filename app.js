eb661dcb67038a13730e501dbb8d1ab68b3c0cbd
Ching0810 <neinchin123@gmail.com>
2023-08-26 11:34:13 +0800

    build: add shortURL result memorizing function

--- app.js
+++ app.js
@@ -21,26 +21,30 @@ app.get('/', (req, res) => {
   res.redirect('/url_shorten')
 })
 
-function random () {
-  arr = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  let str = ''
-  for (let i = 0; i < 5; i++) {
-    str += arr[Math.floor(Math.random() * 62)].toString()
-  }
-  return str
-}
-
 app.get('/url_shorten', (req, res) => {
   res.render('index')
 })
 
+let shortenData = {}
+
 app.post('/url_shorten_result', (req, res) => {
   // Handle the POST request here
-  const submittedUrl = req.body.URLsubmit;
-  // Perform any required logic (e.g., generating a short URL)
-  const shortUrl = random();
-  // Respond to the client with the generated short URL
-  res.render('result', {input_URL: submittedUrl, shorten: shortUrl})
+  const submittedUrl = req.body.URLsubmit
+  // Perform any required logic (e.g., generating a short URL),and respond to the client with the generated short URL
+  if (!Object.keys(shortenData).some((element) => element === submittedUrl)) {
+    const shortUrl = () => {
+      arr = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+      let str = ''
+      for (let i = 0; i < 5; i++) {
+        str += arr[Math.floor(Math.random() * 62)].toString()
+      }
+      return str
+    }
+    shortenData[submittedUrl] = shortUrl()
+    res.render('result', {port: port, shorten: shortenData[submittedUrl], shortenData})
+  } else {
+    res.render('result', {port: port, shorten: shortenData[submittedUrl], shortenData})
+  }
 })
 
 app.listen(port, () => {
