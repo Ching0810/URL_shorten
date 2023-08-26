144a5adc05d3a35cd4d2c3b0a8e63b1cf870d323
Ching0810 <neinchin123@gmail.com>
2023-08-26 15:16:25 +0800

    build: add link copy button function, note: modify variable name

---
+++ public/javascripts/index.js
@@ -0,0 +1,21 @@
+let copyButton = document.querySelector('#copyButton')
+
+// copy link function
+if (copyButton !== null) {
+  copyButton.addEventListener("click", function() {
+    // Define the link you want to copy
+    const linkToCopy = document.querySelector('#shortUrl')
+    
+    const clipboardItem = new ClipboardItem({
+      "text/plain": new Blob([linkToCopy], { type: "text/plain" })
+    })
+    
+    // Copy the ClipboardItem to the clipboard
+    navigator.clipboard.write([clipboardItem]).then(function() {
+      // Change the button text to indicate that the link has been copied
+      document.getElementById("copyButton").textContent = "Link Copied!";
+    }).catch(function(error) {
+      console.error("Unable to copy link:", error);
+    })
+  })
+}
