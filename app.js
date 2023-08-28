eb8d00d4ede9a7ced5ecbcc2f773f7b16ec8e7cf
Ching0810 <neinchin123@gmail.com>
2023-08-28 13:47:25 +0800

    optimize: code optimization

--- app.js
+++ app.js
@@ -27,29 +27,63 @@ app.get('/url_shorten', (req, res) => {
 
 let shortenDataPair = {}
 
+const createShortUrl = () => {
+  arr = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  let str = ''
+  for (let i = 0; i < 5; i++) {
+    str += arr[Math.floor(Math.random() * 62)].toString()
+  }
+  return str
+}
+
+function getKeyByValue(object, value) {
+  for (const key in object) {
+    if (object.hasOwnProperty(key) && object[key] === value) {
+      return key;
+    }
+  }
+}
+
+app.get('/hello', (req, res) => {
+  res.send('hello world')
+})
+
 app.post('/url_shorten_result', (req, res) => {
   // Handle the POST request here
   const clientSubmittedUrl = req.body.URLsubmit
-  // Perform any required logic (e.g., generating a short URL),and respond to the client with the generated short URL
-  if (!Object.keys(shortenDataPair).some((originUrl) => originUrl === clientSubmittedUrl)) {
-    const shortUrl = () => {
-      arr = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-      let str = ''
-      for (let i = 0; i < 5; i++) {
-        str += arr[Math.floor(Math.random() * 62)].toString()
-      }
-      return str
-    }
-    shortenDataPair[clientSubmittedUrl] = shortUrl()
-    res.render('result', {port: port, shorten: shortenDataPair[clientSubmittedUrl], shortenDataPair})
+  let fullUrl = ''
+  if (!Object.values(shortenDataPair).some((url) => url === clientSubmittedUrl)) {
+    const shortUrl = createShortUrl()
+    fullUrl = `${req.headers.origin}/${shortUrl}`
+    shortenDataPair[shortUrl] = clientSubmittedUrl
+    res.render('result', {fullUrl})
   } else {
-    res.render('result', {port: port, shorten: shortenDataPair[clientSubmittedUrl], shortenDataPair})
+    fullUrl = `${req.headers.origin}/${getKeyByValue(shortenDataPair, clientSubmittedUrl)}`
+    res.render('result', {fullUrl})
   }
-  for (const [key, value] of Object.entries(shortenDataPair)) {
-    app.get(`/${value}`, (req, res) => {
-      res.redirect(`${key}`)
-    })
+
+  // Perform any required logic (e.g., generating a short URL),and respond to the client with the generated short URL
+  // if (!Object.keys(shortenDataPair).some((originUrl) => originUrl === clientSubmittedUrl)) {
+    // shortenDataPair[clientSubmittedUrl] = shortUrl()
+  //   shortenDataPair[shortUrl()] = clientSubmittedUrl
+  //   res.render('result', {port: port, shorten: shortenDataPair[shortUrl()], shortenDataPair})
+  // } else {
+  //   res.render('result', {port: port, shorten: shortenDataPair[shortUrl()], shortenDataPair})
+  // }
+  // for (const [key, value] of Object.entries(shortenDataPair)) {
+  //   app.get(`/${value}`, (req, res) => {
+  //     res.redirect(`${key}`)
+  //   })
+  // }
+})
+
+app.get('/:id', (req, res) => {
+  const key = req.params.id;
+  if (shortenDataPair.hasOwnProperty(key)) {
+    const url = shortenDataPair[key]
+    return res.redirect(url)
   }
+  res.redirect('/')
 })
 
 app.listen(port, () => {
