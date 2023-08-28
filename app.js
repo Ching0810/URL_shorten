1898a6e874bb3ac7e12ec45eb34456c0f4eb50ea
Ching0810 <neinchin123@gmail.com>
2023-08-28 16:26:36 +0800

    optimize: store shorten URL results in static json file

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
-// import express from 'express'
-// import { engine } from 'express-handlebars'
-// app is a function
 const app = express()
 const port = 3000
+const myFunction = require('./public/javascripts/myFunction')
+let originData = myFunction.readData('./public/jsons/data.json')
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
-// app.engine('handlebars', engine());
-// app.set('view engine', 'handlebars');
-// app.set('views', './views');
 app.use(express.static('public'))
 // 要用middleware解析結果
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  // req & res are both object, therefore we could use method through .
   res.redirect('/url_shorten')
 })
 
@@ -25,62 +20,25 @@ app.get('/url_shorten', (req, res) => {
   res.render('index')
 })
 
-let shortenDataPair = {}
-
-const createShortUrl = () => {
-  arr = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  let str = ''
-  for (let i = 0; i < 5; i++) {
-    str += arr[Math.floor(Math.random() * 62)].toString()
-  }
-  return str
-}
-
-function getKeyByValue(object, value) {
-  for (const key in object) {
-    if (object.hasOwnProperty(key) && object[key] === value) {
-      return key;
-    }
-  }
-}
-
-app.get('/hello', (req, res) => {
-  res.send('hello world')
-})
-
 app.post('/url_shorten_result', (req, res) => {
   // Handle the POST request here
   const clientSubmittedUrl = req.body.URLsubmit
   let fullUrl = ''
-  if (!Object.values(shortenDataPair).some((url) => url === clientSubmittedUrl)) {
-    const shortUrl = createShortUrl()
+  if (!Object.values(originData).some((url) => url === clientSubmittedUrl)) {
+    const shortUrl = myFunction.createShortUrl()
+    myFunction.storeData(shortUrl, clientSubmittedUrl)
     fullUrl = `${req.headers.origin}/${shortUrl}`
-    shortenDataPair[shortUrl] = clientSubmittedUrl
     res.render('result', {fullUrl})
   } else {
-    fullUrl = `${req.headers.origin}/${getKeyByValue(shortenDataPair, clientSubmittedUrl)}`
+    fullUrl = `${req.headers.origin}/${myFunction.getKeyByValue(originData, clientSubmittedUrl)}`
     res.render('result', {fullUrl})
   }
-
-  // Perform any required logic (e.g., generating a short URL),and respond to the client with the generated short URL
-  // if (!Object.keys(shortenDataPair).some((originUrl) => originUrl === clientSubmittedUrl)) {
-    // shortenDataPair[clientSubmittedUrl] = shortUrl()
-  //   shortenDataPair[shortUrl()] = clientSubmittedUrl
-  //   res.render('result', {port: port, shorten: shortenDataPair[shortUrl()], shortenDataPair})
-  // } else {
-  //   res.render('result', {port: port, shorten: shortenDataPair[shortUrl()], shortenDataPair})
-  // }
-  // for (const [key, value] of Object.entries(shortenDataPair)) {
-  //   app.get(`/${value}`, (req, res) => {
-  //     res.redirect(`${key}`)
-  //   })
-  // }
 })
 
 app.get('/:id', (req, res) => {
   const key = req.params.id;
-  if (shortenDataPair.hasOwnProperty(key)) {
-    const url = shortenDataPair[key]
+  if (originData.hasOwnProperty(key)) {
+    const url = originData[key]
     return res.redirect(url)
   }
   res.redirect('/')
---
+++ public/javascripts/myFunction.js
@@ -0,0 +1,63 @@
+function fs () {
+  const fs = require('fs')
+  return fs
+}
+
+function createShortUrl () {
+  arr = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  let str = ''
+  for (let i = 0; i < 5; i++) {
+    str += arr[Math.floor(Math.random() * 62)].toString()
+  }
+  return str
+}
+
+function getKeyByValue(object, value) {
+  for (const key in object) {
+    if (object.hasOwnProperty(key) && object[key] === value) {
+      return key;
+    }
+  }
+}
+
+function storeData (shortenUrl, originUrl) {
+  fs()
+  // 讀取 data.json 檔案
+  fs.readFile('./public/jsons/data.json', 'utf8', function (err, data) {
+    if (err) {
+      console.error(err)
+      return;
+    }
+    try {
+      // 解析 JSON
+      const jsonData = JSON.parse(data);
+      // 新的資料
+      const newData = {
+        [shortenUrl]: originUrl
+      }
+      // 將新資料新增到 people 陣列
+      jsonData[shortenUrl] = originUrl
+      // 寫回檔案
+      fs.writeFile('./public/jsons/data.json', JSON.stringify(jsonData, null, 2), 'utf8', function (err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log('Data added successfully.');
+      })
+    } catch (jsonError) {
+      console.error(jsonError)
+    }
+  })
+}
+
+function readData (dataPath) {
+  return JSON.parse(fs().readFileSync(dataPath, 'utf8'))
+}
+
+module.exports = {
+  createShortUrl,
+  getKeyByValue,
+  storeData,
+  readData
+}
